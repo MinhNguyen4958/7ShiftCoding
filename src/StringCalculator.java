0622c0d8c72cbb5bc7dfc3b257dc209ee1d9be18
Minh Nguyen <thn649@usask.ca>
2022-02-04 11:59:34 -0600

    Completed request 3

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,13 +1,28 @@
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    public int Add(String numbers) {
+    public int Add(String numbers) throws NumberFormatException {
+
         if (numbers.length() == 0) {
             return 0;
         }
-        String[] num_array = numbers.split(",");
+
+        String[] num_array;
+        if (numbers.startsWith("//")) {
+            String delimiter = numbers.substring(2, 3);
+            String num_string = numbers.substring(4);
+
+            //turn the delimiter into a regex expression
+            num_array = num_string.split(Pattern.quote(delimiter));
+        } else {
+            num_array = numbers.split(",");
+        }
         int sum = 0;
+        String negative = "";
         for (String s : num_array) {
             s = s.replace("\n", "");
-            sum += Integer.parseInt(s);
+            int number = Integer.parseInt(s);
+            sum += number;
         }
         return sum;
     }
@@ -48,6 +63,31 @@ public class StringCalculator {
             System.out.println("error in the return value!");
             failed_count++;
         }
+
+        //Request 3
+        //Test case 1
+        expected = 8;
+        result = calculator.Add("//;\n1;3;4");
+        if (result != expected) {
+            System.out.println("error in the return value!");
+            failed_count++;
+        }
+
+        //Test case 2
+        expected = 6;
+        result = calculator.Add("//$\n1$2$3");
+        if (result != expected) {
+            System.out.println("error in the return value!");
+            failed_count++;
+        }
+        //Test case 3
+        expected = 13;
+        result = calculator.Add("//@\n2@3@8");
+        if (result != expected) {
+            System.out.println("error in the return value!");
+            failed_count++;
+        }
+
         System.out.println("Test completed: " + failed_count + " failed cases");
     }
 }
