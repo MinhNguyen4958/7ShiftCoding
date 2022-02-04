5ff91ebad8ccfe4d095ef48eb03820f20583ff1c
Minh Nguyen <thn649@usask.ca>
2022-02-04 11:09:13 -0600

    Completed request 2

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -6,6 +6,7 @@ public class StringCalculator {
         String[] num_array = numbers.split(",");
         int sum = 0;
         for (String s : num_array) {
+            s = s.replace("\n", "");
             sum += Integer.parseInt(s);
         }
         return sum;
@@ -14,6 +15,7 @@ public class StringCalculator {
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
         int failed_count = 0;
+        // Request 1
         // Test case 1 - empty String
         int expected = 0;
         int result = calculator.Add("");
@@ -22,14 +24,30 @@ public class StringCalculator {
             failed_count++;
         }
 
-         expected = 14;
+        //Test case 2
+        expected = 14;
         result = calculator.Add("5,6,3");
         if (result != expected) {
             System.out.println("error in the return value!");
             failed_count++;
         }
 
+        //Request 2
+        //Test case 1
+        expected = 6;
+        result = calculator.Add("1\n,2,3");
+        if (result != expected) {
+            System.out.println("error in the return value!");
+            failed_count++;
+        }
 
+        //Test case 2
+        expected = 7;
+        result = calculator.Add("1,\n2,4");
+        if (result != expected) {
+            System.out.println("error in the return value!");
+            failed_count++;
+        }
         System.out.println("Test completed: " + failed_count + " failed cases");
     }
 }
